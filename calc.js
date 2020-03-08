c3b8cd44a417650786fddd4666e95d5025695d41
Douglas Murita <douglasb008@gmail.com>
2020-03-08 02:09:12 -0300

    arquivos da calculadora em js

---
+++ calc.js
@@ -0,0 +1,28 @@
+
+function somar(){
+       var n1=parseInt(document.getElementById("resultado")).value;
+       var n2=parseInt(document.getElementById("resultado")).value;
+       var soma = n1 + n2;
+}
+
+function subtrair(){
+    var n1=parseInt(document.getElementById("resultado")).value;
+    var n2=parseInt(document.getElementById("resultado")).value;
+    var soma = n1 - n2;
+}
+
+function dividir(){
+    var n1=parseInt(document.getElementById("resultado")).value;
+    var n2=parseInt(document.getElementById("resultado")).value;
+    var soma = n1 / n2;
+}
+
+function multiplicar(){
+    var n1=parseInt(document.getElementById("resultado")).value;
+    var n2=parseInt(document.getElementById("resultado")).value;
+    var soma = n1 * n2;
+}
+
+function resultado(){
+    
+}
